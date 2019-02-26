b998c199a7f2bb0e86a3e885567f1aa68b1c385a
tlsdba <sei_th2010@126.com>
2019-02-26 16:59:23 -0500

    modified python

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "test " << argv[1] << endl;
+    string str = argv[1];
+    str += ".txt";
+    ofstream out_put(str.c_str());
+    out_put << "test " << argv[1] << endl;
+    out_put.close();
     return 0;
 }
 
